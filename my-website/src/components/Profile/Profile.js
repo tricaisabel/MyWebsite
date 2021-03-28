dc6d888de4cd99e8c6b48f3517b43dbc3f272139
Isabel <isabel.claudia31@gmail.com>
2021-03-28 13:51:20 +0300

    Name, Title, Profile picture
    I added the name, title, and profile picture sections from the profile component

--- my-website/src/components/Profile/Profile.js
+++ my-website/src/components/Profile/Profile.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Typography} from '@material-ui/core';
+import './Profile.css';
+import profile from '../../assets/images/picture.jpg';
 
 const Profile = () => {
     return (
-        <div>
-            This is a profile component
+        <div className='profile container_shadow'>
+            <div className='profile_name'>
+            <Typography className='name'>Isabel</Typography>
+            <Typography className='title'>Title</Typography>
+            </div>        
+        <figure className='profile_image'>
+            <img src={profile} alt="" />
+        </figure>
+
+        <div className='profile_information'>
+            Insert timeline<br/>
+            <button>my button</button>
         </div>
+        </div>
+
+
     )
 }
 
