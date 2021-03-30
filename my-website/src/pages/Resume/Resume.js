6a17018d6dc802d3ae654edf093495183cce596f
Isabel <isabel.claudia31@gmail.com>
2021-03-30 16:51:44 +0300

    Added a timeline for education and experience

--- my-website/src/pages/Resume/Resume.js
+++ my-website/src/pages/Resume/Resume.js
@@ -2,6 +2,16 @@ import React from 'react'
 import './Resume.css'
 import {Container, Grid, Typography} from '@material-ui/core';
 import resumeData from '../../utils/resumeData';
+import CustomTimeline, {CustomTimelineSeparator} from '../../components/Timeline/Timeline';
+import WorkIcon from '@material-ui/icons/Work';
+import SchoolIcon from '@material-ui/icons/School';
+import TimelineItem from '@material-ui/lab/TimelineItem';
+import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
+import TimelineConnector from '@material-ui/lab/TimelineConnector';
+import TimelineDot from '@material-ui/lab/TimelineDot';
+import TimelineContent from '@material-ui/lab/TimelineContent';
+
+
 const Resume = () => {
     return (
         //
@@ -17,7 +27,59 @@ const Resume = () => {
             </Grid>
         </Grid>
         {/*Education and Experiences*/}
-        <Grid container className='section'></Grid> 
+        <Grid container className='section'>
+            <Grid item className='section_title mb_30'>
+                <span></span>
+                <h5 className='section_title_text'>Resume</h5>
+            </Grid> 
+            <Grid item xs={12}>
+                <Grid container className='resume_timeline'>
+                    {/*Experiences*/}
+                    <Grid item sm={12} md={6}>
+                        <CustomTimeline title='Work Experience' icon={<WorkIcon />}>
+                            {resumeData.experiences.map(experience =>  (
+                                <TimelineItem>
+                                    <TimelineSeparator className='separator_padding'>
+                                        <TimelineDot 
+                                            varinat='outlined'
+                                            className='timeline_dot'
+                                        />
+                                        <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <TimelineContent className='timeline_content'>
+                                        <Typography varinat='h2' className='timeline_title'>{experience.title}</Typography>
+                                        <Typography variant='caption' className='timeline_date'>{experience.date}</Typography>
+                                        <Typography variant='body2' className='timeline_description'>{experience.description}</Typography>
+                                    </TimelineContent>
+                                </TimelineItem>
+                            ))}
+                        </CustomTimeline>
+                    </Grid>
+                    
+                    {/*Education*/}
+                    <Grid item sm={12} md={6}>
+                        <CustomTimeline title='Education' icon={<SchoolIcon />}>
+                            {resumeData.educations.map(education =>  (
+                                <TimelineItem>
+                                    <TimelineSeparator className='separator_padding'>
+                                        <TimelineDot 
+                                            varinat='outlined'
+                                            className='timeline_dot'
+                                        />
+                                        <TimelineConnector />
+                                    </TimelineSeparator>
+                                    <TimelineContent className='timeline_content'>
+                                        <Typography varinat='h2' className='timeline_title'>{education.title}</Typography>
+                                        <Typography variant='caption' className='timeline_date'>{education.date}</Typography>
+                                        <Typography variant='body2' className='timeline_description'>{education.description}</Typography>
+                                    </TimelineContent>
+                                </TimelineItem>
+                            ))}
+                        </CustomTimeline>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Grid> 
         {/*Services*/}
         <Grid container className='section'></Grid> 
         {/*Skills*/}
--- my-website/src/utils/resumeData.js
+++ my-website/src/utils/resumeData.js
@@ -33,6 +33,39 @@ export default {
         },
     },
 
-    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque accumsan, orci ac vulputate laoreet, augue risus fringilla tellus, vel laoreet nisl magna eu risus. Nam in ultricies tellus. Etiam vehicula commodo sem. Nullam non suscipit turpis. Nullam mattis mauris vitae leo suscipit tincidunt. Nam porttitor euismod ligula vitae condimentum. Nulla accumsan odio at sapien gravida vestibulum. Ut metus diam, fermentum blandit nulla id, fringilla commodo justo. Quisque nisi nibh, elementum a lobortis et, vulputate et ante.\n\n Aliquam feugiat mi dolor, nec accumsan dolor tristique eget. Suspendisse pellentesque lobortis quam, eu gravida libero suscipit vel. Duis rhoncus orci nec tortor egestas rutrum. Nunc quis ex sit amet ante accumsan vulputate. Nulla in pulvinar justo. Nullam sed facilisis enim. Maecenas hendrerit, augue non placerat blandit, mi risus scelerisque magna, mollis facilisis mi ipsum sed arcu. Morbi lobortis tincidunt enim ac elementum."
-    
+    about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque accumsan, orci ac vulputate laoreet, augue risus fringilla tellus, vel laoreet nisl magna eu risus. Nam in ultricies tellus. Etiam vehicula commodo sem. Nullam non suscipit turpis. Nullam mattis mauris vitae leo suscipit tincidunt. Nam porttitor euismod ligula vitae condimentum. Nulla accumsan odio at sapien gravida vestibulum. Ut metus diam, fermentum blandit nulla id, fringilla commodo justo. Quisque nisi nibh, elementum a lobortis et, vulputate et ante.\n\n Aliquam feugiat mi dolor, nec accumsan dolor tristique eget. Suspendisse pellentesque lobortis quam, eu gravida libero suscipit vel. Duis rhoncus orci nec tortor egestas rutrum. Nunc quis ex sit amet ante accumsan vulputate. Nulla in pulvinar justo. Nullam sed facilisis enim. Maecenas hendrerit, augue non placerat blandit, mi risus scelerisque magna, mollis facilisis mi ipsum sed arcu. Morbi lobortis tincidunt enim ac elementum.",
+    experiences: [
+    {
+        title:'Work 1',
+        date:'2018 - Prezent',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tortor lectus, elementum eget sodales vel, sagittis eget odio. Morbi pretium facilisis enim vitae viverra. Etiam elementum orci vel rutrum fringilla.'
+
+    },
+    {
+        title:'Work 2',
+        date:'2019 - Prezent',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tortor lectus, elementum eget sodales vel, sagittis eget odio. Morbi pretium facilisis enim vitae viverra. Etiam elementum orci vel rutrum fringilla.'
+
+    },
+    {
+        title:'Work 3',
+        date:'2020 - Prezent',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tortor lectus, elementum eget sodales vel, sagittis eget odio. Morbi pretium facilisis enim vitae viverra. Etiam elementum orci vel rutrum fringilla.'
+
+    },
+ ],
+ educations: [
+    {
+        title:'Education 1',
+        date:'2018 - Prezent',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tortor lectus, elementum eget sodales vel, sagittis eget odio. Morbi pretium facilisis enim vitae viverra.'
+
+    },
+    {
+        title:'Education 2',
+        date:'2019 - Prezent',
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tortor lectus, elementum eget sodales vel, sagittis eget odio. Morbi pretium facilisis enim vitae viverra.'
+
+    }
+ ]
 };
